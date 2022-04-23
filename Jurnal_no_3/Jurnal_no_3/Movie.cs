e6df335d62dc0a6f5395225ca28406cb9c4c58a5
ANITA SITUMORANG <situmoranganita@365.telkomuniversity.ac.id>
2022-04-23 17:48:04 +0700

    JURNAL_MOD8_KPL

---
+++ Jurnal_no_3/Jurnal_no_3/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jurnal_no_3
+{
+    public class Movies
+    {
+        private string Title { get; set; }
+        private string Director { get; set; }
+        private List<string> Stars { get; set; }
+        private String Description { get; set; }
+    }
+}
